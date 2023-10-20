5efe8559faa9a7e3469d19aa20669fd7333a22fb
iranilsj <irani.fpo@gmail.com>
2023-10-20 01:29:56 -0300

    Tipos de console

---
+++ console.api.js
@@ -0,0 +1,12 @@
+// log -> Registro
+
+const minhaVar = true;
+
+console.log(245);
+console.log("Eu sou um texto");
+console.log(minhaVar);
+
+// Tratamento de erro!
+
+console.error("Deu erro!")
+console.error(new  Error("Deu erro!"))
