73975f513a0273953262d817b482f6cf3062b33d
iranilsj <irani.fpo@gmail.com>
2023-10-20 01:25:42 -0300

    exemplo de tipagem em JavaScript

---
+++ tipagem.js
@@ -0,0 +1,13 @@
+// Tipagem dinâmica
+// Untyped
+// Multparadiggma
+// ES6 / ES2015 -> Versoes 
+
+let minhaVar = 567;
+console.log(minhaVar);
+
+minhaVar  = "Texto";
+console.log(minhaVar);
+
+minhaVar = true;
+console.log(minhaVar);
