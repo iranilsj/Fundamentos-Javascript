ea24b5782becfa464f55ecedbf86c2c14b38f38c
iranilsj <irani.fpo@gmail.com>
2023-10-20 01:28:12 -0300

    Exemplo de erros

---
+++ erros-e-stacktrace.js
@@ -0,0 +1,16 @@
+// Erro Sintax
+const numero;
+// Resolvido
+const numero = 0;
+
+// Erro sintax
+console.log(numero
+//Resolvido
+console.log(numero);
+
+//Reference -> Varriavel não definida (minhaVar)
+console.log(minhaVar);
+//Resolvido
+const minhaVar = "valor"
+console.log(minhaVar);
+
