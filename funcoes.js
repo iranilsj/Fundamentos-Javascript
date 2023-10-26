a91cd41f54f8961afa1ce4355d1fd94cd1924f18
iranilsj <irani.fpo@gmail.com>
2023-10-26 17:30:30 -0300

    Algumas funções

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+// let x = ""
+// console.log(x)
+// x = "Oi"
+
+// 01 - Declara função
+                     //String
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+// 2  - Executa a função (1 ou + vezes)
+imprimeTexto("Oi")
+imprimeTexto("Outro texto")
+
+//  três formas de escrever funções
+
+function soma(){
+    // antes do (return)
+    //outros codigos
+    // vários console.log()
+    return 2 + 2 
+}
+console.log(soma())
